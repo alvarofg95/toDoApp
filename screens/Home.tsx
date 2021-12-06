f444d681401833574bebf1ae5be09db9036f6b9b
alvarofg95 <alvarofg95dev@gmail.com>
2021-12-07 00:11:07 +0100

    add button and text

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import {View, StyleSheet, FlatList} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  TextInput,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
 import TaskList from '../components/task-list/default';
 
 const data = [
@@ -18,9 +25,25 @@ const data = [
 ];
 
 const Home: React.FC = () => {
+  const [value, setValue] = useState<string>('');
+
+  const handleChange = (newValue: string) => {
+    setValue(newValue);
+  };
+
   return (
     <View style={styles.container}>
       <FlatList data={data} renderItem={item => <TaskList {...item} />} />
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={value}
+          style={styles.input}
+          onChangeText={handleChange}
+        />
+        <TouchableOpacity style={styles.addButton}>
+          <Text style={styles.icon}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -29,6 +52,32 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#1A1A1F',
     height: '100%',
+  },
+  inputContainer: {
+    margin: 10,
+    marginBottom: 20,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  input: {
+    backgroundColor: 'white',
+    width: '80%',
+    borderRadius: 10,
+    padding: 10,
+    fontSize: 15,
+  },
+  icon: {
+    marginTop: -2,
+    fontSize: 35,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+  addButton: {
+    backgroundColor: 'white',
+    width: 50,
+    height: 50,
+    borderRadius: 25,
   },
 });
 
